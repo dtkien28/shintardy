b89464abe5c9fe36a5f66a17a158d0f63b104e7b
DangKien28 <trungkien228102005@gmail.com>
2026-07-10 21:06:50 +0700

    new import

--- apps/api/src/modules/stories/stories.routes.js
+++ apps/api/src/modules/stories/stories.routes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const storyController = require('./stories.controller');
-const { protect } = require('../../middlewares/auth.middleware');
+const protect = require('../../middlewares/auth.middleware');
 
 // Public/Member routes
 router.get('/today', storyController.getTodayStories);
